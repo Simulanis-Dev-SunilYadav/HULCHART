07caf7748ad666ae491ead4f225723bfb053ad3a
sunil yadav <sunilyadav@simulanis.com>
2023-02-14 12:33:26 +0530

    more charts add

--- src/Components/charts/nmbprocesscapability/Cp.js
+++ src/Components/charts/nmbprocesscapability/Cp.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 import Chart from 'react-apexcharts'
 
@@ -7,7 +8,8 @@ export class Cp extends Component {
           this.state = {
             series: [44, 55, 13],
             options: {
-              colors: ['#e5b52d', '#3ca55f', '#d50707'],
+              // colors: ['#e5b52d', '#3ca55f', '#d50707'],
+              colors: ['#d50707', '#e5b52d', '#3ca55f'],
               chart: {
                 width: 380,
                 type: 'pie',
@@ -16,7 +18,8 @@ export class Cp extends Component {
                     position: 'bottom',
                     show:false
                 },
-              labels: ['Team A', 'Team B', 'Team C'],
+              // labels: ['Team A', 'Team B', 'Team C'],
+              labels: ['<1', '>1.33', '1-1.33'],
               responsive: [{
                 breakpoint: 480,
                 options: {
@@ -32,6 +35,35 @@ export class Cp extends Component {
             },
           };
         }
+
+
+
+         componentDidMount(){
+           let current = this;
+       const passHeader = {
+          Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwYmJiNzRjNy02NTdlLTRlM2QtYTZiZC00ZTcxNTQwYWUyMDIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vZjY2ZmFlMDItNWQzNi00OTViLWJmZTAtNzhhNmZmOWY4ZTZlL3YyLjAiLCJpYXQiOjE2NzYwMDY5MTksIm5iZiI6MTY3NjAwNjkxOSwiZXhwIjoxNjc2MDEwODE5LCJhaW8iOiJBVFFBeS84VEFBQUE5QnEwVUdMWnB3c0NSdG82V0M5eVBUa3FaV0s5aTJlUE9HcG9XOThEUk54Mnl5alZWWUdqRGNnenlrNlIvdXhBIiwibmFtZSI6Iml0LCBkYXBhZGEiLCJub25jZSI6IjRjODE2YTIxLTk3MDgtNGNkNy1hOWY0LTBhNmI4NmU2MjdmMyIsIm9pZCI6IjQ5N2EzYTg0LWQ3NDEtNGYyMi05OTkyLTI4MmJkZjk1YTQ4NyIsInByZWZlcnJlZF91c2VybmFtZSI6ImRhcGFkYS5pdEB1bmlsZXZlci5jb20iLCJyaCI6IjAuQVF3QUFxNXY5alpkVzBtXzRIaW1fNS1PYnNkMHV3dC1aVDFPcHIxT2NWUUs0Z0lNQUJ3LiIsInN1YiI6IlV0cE41YVBEckdtQkI1NTFucVBvRDNhWG5XZXVUSFg1SFZndVduNi1nOEUiLCJ0aWQiOiJmNjZmYWUwMi01ZDM2LTQ5NWItYmZlMC03OGE2ZmY5ZjhlNmUiLCJ1dGkiOiJpeExteV9QOFdFeThQVWJodDV6LUFBIiwidmVyIjoiMi4wIn0.otI1gusOVie2HnsYjqjZvdlfP3KOaCct-2-5Mtgq8-m_Lqt8hR46ZtitdbOHHwBmSUtqpu_BxdFE9gIYERZTt-Z27yaat6WBk72XxhXeM7W9ScvauDk3XFrObpW51HjMlCfxx3dopuq4uiIoWJTy7E5zdGAq5gfu10P9qij6bmdQQqC_ACKHUsnHqZJJofN3qSCCGPX7EvYJjTk5uCTOK9s5-wpSnIBvrk_QhnKqhTKUg_hwlHWzikcmImjI59iMpAbxJLRp7FGZoFDO3-x32ACHtI3Zcu_0ZWxho4AB-Dwkr3dq8y_XgcAGXzL5cyiL9IQ7WB7Kq-5zvN1czmQT2A",
+          Accept: "application/json",
+            "Content-Type": "application/json",
+              };
+              axios.get(`https://bnlwe-gs-d-57321-apimgt.azure-api.net/nmbapi/GetKPPKBP?date=02/10/2023` , {
+                headers: passHeader,
+                }).then((response) =>{
+                  console.log("-----response")
+                  console.log(response.data.overAllKPP.totalKppCpValue);
+                  var valueArray = [response.data.overAllKPP.totalKppCpValue.red,   response.data.overAllKPP.totalKppCpValue.amber,  response.data.overAllKPP.totalKppCpValue.green]
+                  current.setState({
+                    series: valueArray
+                  })
+                }).catch((err)=>{
+                   console.log("-----err")
+                  console.log(err)
+                })
+
+    }
+
+
+
+
   render() {
     return (
       <>
